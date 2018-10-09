45313af8ad1e0e7e52ea9a29f5d396cc76bc996d
Maya <maya.husic@gmail.com>
2018-10-09 09:52:16 -0400

    Grammar mistakes corrected

--- pickNum.js
+++ pickNum.js
@@ -1,16 +1,16 @@
 var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 10) + 1;
-var guess = prompt('I am thinking of a number between 1 anf 10, can you guess the number?');
+var guess = prompt('I am thinking of a number between 1 and 10, can you guess the number?');
 // if (parseInt(guess) === randomNumber) {
 //  document.write('<h2>Bravo, you guessed the number!</h2>')
 //} else {
-//  document.write('<h2>Sorry but that is not a correct number. The number I was thinking of is ' + randomNumber + ' </h2>')
+//  document.write('<h2>Sorry but that is not the correct number. The number I was thinking of is ' + randomNumber + ' </h2>')
 //}
 if (parseInt(guess) === randomNumber) {
   correctGuess = true;
 }
-if (correctGuess === ture) {
+if (correctGuess === true) {
   document.write("<h2>You've guess the number!</h2>");
 } else {
-  document.write("<h2>Sorry but that is not a correct number. The number I was thinking of was " + randomNumber + " </h2>");
+  document.write("<h2>Sorry but that is not the correct number. The number I was thinking of was " + randomNumber + " </h2>");
 }
